b4c204781e69f0aa24a1517de1716305af5912db
ADGandro <157210375+ADGandro@users.noreply.github.com>
2024-04-26 21:40:56 +0400

    Add files via upload

---
+++ დავალება 1.py
@@ -0,0 +1,23 @@
+import random
+
+
+a = random.randint(1, 100)
+b = random.randint(1, 100)
+
+print("a:", a)
+print("b:", b)
+
+if a % 2 == 0 and b % 2 == 0:
+    result = a ** b
+    print("a ** b =", result)
+
+
+elif a % 2 == 0 and b % 2 != 0:
+    result = a * b
+    print("a * b =", result) 
+
+elif a % 2 != 0 and b % 2 != 0:
+    c = random.randint(1, 100)
+    print("c", c)
+    result = a * b * c
+    print("a * b * c =", result)
